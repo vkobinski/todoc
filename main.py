1f7e64be0688ae5191120afb8fbf89da204ee9ca
vkobinski <victorkobinski@Outlook.com>
2022-01-02 22:04:36 -0300

    Added Remove Cmd

--- main.py
+++ main.py
@@ -11,17 +11,29 @@ def todoByImportance(todos):
         current = Todo(todo)
         printList.append(current)
     printList.sort(key=lambda x: x.importance, reverse=True)
-    print("You have {0} TODOS:".format(len(printList)))
     for todo in printList:
         current_index += 1
-        print("{0}° {1}: {2}".format(current_index,todo.prefix, todo.body))
+        print("({0}) {1}: {2}".format(current_index,todo.prefix, todo.body))
+    return printList
+
+def printTodoCount(printList):
+    print("You have {0} TODOS:".format(len(printList)))
+
 
 def usage():
     print("Usage: todoc <SUBCOMMAND>")
     print("SUBCOMMANDS:")
     print("    list        List All Your TODOs")
     print("    add         Add a TODO")
-    print("    complete    Mark a TODO as Completed")
+    print("    remove      Mark a TODO as Completed")
+
+def listFromFile(caminho):
+    todos = []
+    mode = 'r' if os.path.exists(caminho) else 'a+'
+    with open(caminho, mode) as file:
+        for line in file:
+            todos.append(line.replace("\n"," "))
+    return todos
 
 if len(sys.argv) < 2:
     print("Not enough arguments")
@@ -30,11 +42,8 @@ if len(sys.argv) < 2:
 else:
     caminho = os.path.expanduser("~/todo.txt")
     if tokens[0] == 'list':
-        todos = []
-        mode = 'r' if os.path.exists(caminho) else 'a+'
-        with open(caminho, mode) as file:
-            for line in file:
-                todos.append(line.replace("\n"," "))
+        todos = listFromFile(caminho)
+        printTodoCount(todos)
         todoByImportance(todos)
     if tokens[0] == 'add':
         prefix = int(input("Insert the TODO importance (1-9): "))
@@ -42,6 +51,15 @@ else:
         final_body = "{0}: {1} \n".format("TOD"+ ("O"*prefix), body)
         with open(caminho, 'a+') as file:
             file.write(final_body)
-    if tokens[0] == 'complete':
-        # TODOO(#1): Implement the Complete Arg
-        assert False, "Not yet implemented"
+    if tokens[0] == 'remove':
+        # TODOO(#1): Implement the Remove Arg
+        todos = listFromFile(caminho)
+        printTodoCount(todos)
+        todos = todoByImportance(todos)
+        remove = int(input("Insert the TODO index you want to remove: "))
+        todos.pop(remove-1)
+        with open(caminho, 'w') as file:
+            for todo in todos:
+                todo_prefix = todo.prefix + ("O" * todo.importance)
+                final_body = "{0}: {1} \n".format(todo_prefix, todo.body)
+                file.write(final_body)
